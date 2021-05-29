9d0c2ae0c9735fac76cfda9ed9562878e6f7139f
Eric Quelch <74473426+quelchx@users.noreply.github.com>
2021-05-28 20:02:45 -0400

    spelling error 🚀

--- components/utils/Loading.vue
+++ components/utils/Loading.vue
@@ -2,8 +2,8 @@
   <div>
     <progress class="progress is-small is-link" max="100"></progress>
     <p>
-      Im a poor developer in a big sea -- making his way. So we waiting on
-      Heroku and MongoDB since I can only afford the free tier...
+      Im a poor developer in a big sea -- making his way. So we might be waiting on
+      Heroku and MongoDB since I can only afford the free tier 🚀
     </p>
   </div>
 </template>
